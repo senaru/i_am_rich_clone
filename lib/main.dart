c2a3af764da0465fbd7cb17b2f6f22603263652e
Senaru <s3696653@student.rmit.edu.au>
2020-07-22 13:14:05 +1000

    Scaffolding and trying a network image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://miro.medium.com/max/1200/1*Qc0XxYm-qAZL-7tjjlNfrg.png'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
